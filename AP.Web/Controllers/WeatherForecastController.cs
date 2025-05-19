1f5759b489faa49b2e596d65569be2313011344e
Ashish Shende <ashish.shende@emergys.com>
2025-05-19 11:33:54 +0530

    API Key added

--- AP.Web/Controllers/WeatherForecastController.cs
+++ AP.Web/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private string API_KEY = "47a383dca7d0ac4ec3526145ea3f51e5cfeac8d2";
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -21,9 +22,10 @@ namespace WebApplication1.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            Console.WriteLine("API KEY" + this.API_KEY);
+            return Enumerable.Range(1, 5).Select(Index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(Index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
